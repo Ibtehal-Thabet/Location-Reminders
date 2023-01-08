ce9c75b578d2d1e5dc978a61ef6641aec958ef10
Ibtehal-Thabet <55961675+Ibtehal-Thabet@users.noreply.github.com>
2023-01-09 01:49:12 +0200

    Add files via upload

---
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -0,0 +1,50 @@
+package com.udacity.project4.locationreminders.data
+
+import androidx.lifecycle.MutableLiveData
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.dto.Result
+import java.lang.Error
+import java.util.LinkedHashMap
+
+//Use FakeDataSource that acts as a test double to the LocalDataSource
+class FakeDataSource(var reminders: MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
+
+//   Create a fake data source to act as a double to the real data source
+
+    private var shouldReturnError = false
+
+    fun setReturnError(value: Boolean) {
+        shouldReturnError = value
+    }
+
+    override suspend fun getReminders(): Result<List<ReminderDTO>> {
+        if (shouldReturnError)
+            return Result.Error("Reminder exception")
+        else
+            return Result.Success(ArrayList(reminders))
+
+    }
+
+    override suspend fun saveReminder(reminder: ReminderDTO) {
+        reminders?.add(reminder)
+    }
+
+    override suspend fun getReminder(id: String): Result<ReminderDTO> {
+        if (shouldReturnError) {
+            return Result.Error("Reminder exception")
+        }
+
+        val reminder = reminders?.find { task ->
+            task.id == id
+        }
+
+        if(reminder != null)
+            return Result.Success(reminder)
+
+        return Result.Error("Reminder not found")
+    }
+
+    override suspend fun deleteAllReminders() {
+        reminders?.clear()
+    }
+}
